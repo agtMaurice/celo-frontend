eff220f2ac6702b9a64b37e91611b4ad121774f1
agtMaurice <agtmaurice@gmail.com>
2023-08-10 00:10:40 +0100

    fixes

--- packages/react-app/components/ProductList.tsx
+++ packages/react-app/components/ProductList.tsx
@@ -12,56 +12,72 @@ import SuccessAlert from "@/components/alerts/SuccessAlert";
 
 // Define the ProductList component
 const ProductList = () => {
-    // Use the useContractCall hook to read how many products are in the marketplace contract
-    const { data } = useContractCall("getProductsLength", [], true);
-    // Convert the data to a number
-    const productLength = data ? Number(data.toString()) : 0;
-    // Define the states to store the error, success and loading messages
-    const [error, setError] = useState("");
-    const [success, setSuccess] = useState("");
-    const [loading, setLoading] = useState("");
-    // Define a function to clear the error, success and loading states
-    const clear = () => {
-      setError("");
-      setSuccess("");
-      setLoading("");
-    };
-    // Define a function to return the products
-    const getProducts = () => {
-      // If there are no products, return null
-      if (!productLength) return null;
-      const products = [];
-      // Loop through the products, return the Product component and push it to the products array
-      for (let i = 0; i < productLength; i++) {
-        products.push(
-          <Product
-            key={i}
-            id={i}
-            setSuccess={setSuccess}
-            setError={setError}
-            setLoading={setLoading}
-            loading={loading}
-            clear={clear}
-          />
-        );
-      }
-      return products;
-    };
+  // Use the useContractCall hook to read how many products are in the marketplace contract
+  const { data } = useContractCall("getProductsLength", [], true);
+  // Convert the data to a number
+  const productLength = data ? Number(data.toString()) : 0;
+  // Define the states to store the error, success and loading messages
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [loading, setLoading] = useState("");
+  const [view, setView] = useState<"grid" | "list">("grid"); // State for view type
 
-    // Return the JSX for the component
+  // Define a function to clear the error, success and loading states
+  const clear = () => {
+    setError("");
+    setSuccess("");
+    setLoading("");
+  };
+
+  // Define a function to return the products
+  const getProducts = () => {
+    // If there are no products, return null
+    if (!productLength) return null;
+    const products = [];
+    // Loop through the products, return the Product component and push it to the products array
+    for (let i = 0; i < productLength; i++) {
+      products.push(
+        <Product
+          key={i}
+          id={i}
+          view={view} // Passing the view to Product component
+          setSuccess={setSuccess}
+          setError={setError}
+          setLoading={setLoading}
+          loading={loading}
+          clear={clear}
+        />
+      );
+    }
+    return products;
+  };
+
+  // Return the JSX for the component
   return (
     <div>
       {/* If there is an alert, display it */}
       {error && <ErrorAlert message={error} clear={clear} />}
       {success && <SuccessAlert message={success} />}
       {loading && <LoadingAlert message={loading} />}
+      {/* Toggle button */}
+      <button
+    onClick={() =>
+      setView((prevView) => (prevView === "grid" ? "list" : "grid"))
+    }
+    className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-150 ease-in-out"
+>
+    Toggle View
+</button>
+
       {/* Display the products */}
-      <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h2 className="sr-only">Products</h2>
-        <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
-          {/* Loop through the products and return the Product component */}
-          {getProducts()}
-        </div>
+      <div
+        className={`mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8 ${
+          view === "grid"
+            ? "grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8"
+            : "grid grid-cols-1 gap-y-6"
+        }`}
+      >
+        {getProducts()}
       </div>
     </div>
   );
